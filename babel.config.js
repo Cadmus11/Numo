2954dd573eb7c8f7b9666afbaf002840626ac839
cadmus11 <obsidian.north.003@gmail.com>
2026-06-28 17:39:41 +0300

    json fix II

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,44 @@
+// module.exports = function (api) {
+//   api.cache(true);
+//   let plugins = [];
+
+//   plugins.push('react-native-worklets/plugin');
+
+//   return {
+//     presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
+
+//     plugins,
+//   };
+// };
+
+
+// module.exports = function (api) {
+//   api.cache(true);
+
+//   return {
+//     presets: [
+//       ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
+//       'nativewind/babel',
+//     ],
+//     plugins: [
+//       'react-native-worklets/plugin',
+//       'react-native-reanimated/plugin',
+//     ],
+//   };
+// };
+
+//CQBP-NNPJ cline code
+
 module.exports = function (api) {
   api.cache(true);
-  let plugins = [];
-
-  plugins.push('react-native-worklets/plugin');
 
   return {
-    presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
-
-    plugins,
+    presets: [
+      ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
+      'nativewind/babel',
+    ],
+    plugins: [
+      'react-native-reanimated/plugin',
+    ],
   };
 };
